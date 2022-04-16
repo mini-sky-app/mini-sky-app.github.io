866bff8754a74693a3ba1d652bff9166b8fe6c44
Zhenyu Liang <65879206+Zhenyu-Liang@users.noreply.github.com>
2022-04-16 13:03:55 -0400

    added weather data

--- sky.js
+++ sky.js
@@ -1,79 +1,78 @@
-let nightSky = document.querySelector('#sky-night');
-let nightOcean = document.querySelector('#ocean-night');
-let sun = document.querySelector('#sun');
-let glow = document.querySelector('#glow');
-let clock = document.querySelector('#clock');
+let nightSky = document.querySelector("#sky-night");
+let nightOcean = document.querySelector("#ocean-night");
+let sun = document.querySelector("#sun");
+let glow = document.querySelector("#glow");
+let clock = document.querySelector("#clock");
 
-function setSunAngle(angle){
-    sun.style.transform = `translate(-50%, -50%) rotate(${angle}rad) translateY(-22vmin)`;
-    setDayNightTransition(1 - (Math.cos(angle)*2/3 + 1/3));
-    setGlow(1-Math.cos(2 * angle)-1);
+function setSunAngle(angle) {
+  sun.style.transform = `translate(-50%, -50%) rotate(${angle}rad) translateY(-22vmin)`;
+  setDayNightTransition(1 - ((Math.cos(angle) * 2) / 3 + 1 / 3));
+  setGlow(1 - Math.cos(2 * angle) - 1);
 }
 
-function setDayNightTransition(opacity){
-    nightSky.style.opacity = opacity;
-    nightOcean.style.opacity = opacity;
+function setDayNightTransition(opacity) {
+  nightSky.style.opacity = opacity;
+  nightOcean.style.opacity = opacity;
 }
 
-function setGlow(opacity){
-    glow.style.opacity = opacity;
+function setGlow(opacity) {
+  glow.style.opacity = opacity;
 }
 
 //function add cloud adds div with class 'cloud', starts at left 100%, moves left with transition. vertical position is random.
-function addCloud(speed){
-    speed = speed * (Math.random() * 0.5 + 0.5);
-    let cloud = document.createElement('div');
-    cloud.classList.add('cloud');
-    cloud.style.left = '140%';
-    cloud.style.width = `${Math.random()*50 + 10}%`;
-    cloud.style.top = `${Math.random()*50}%`;
-    cloud.style.transition = `left ${speed}s linear`;
-    document.querySelector('#center').appendChild(cloud);
+function addCloud(speed) {
+  speed = speed * (Math.random() * 0.5 + 0.5);
+  let cloud = document.createElement("div");
+  cloud.classList.add("cloud");
+  cloud.style.left = "140%";
+  cloud.style.width = `${Math.random() * 50 + 10}%`;
+  cloud.style.top = `${Math.random() * 50}%`;
+  cloud.style.transition = `left ${speed}s linear`;
+  document.querySelector("#center").appendChild(cloud);
 
-    requestAnimationFrame(() => {
-        cloud.style.left = '-100%';
-    });
+  requestAnimationFrame(() => {
+    cloud.style.left = "-100%";
+  });
 
-    setTimeout(() => {
-        cloud.remove();
-    }, speed*1000*2);
+  setTimeout(() => {
+    cloud.remove();
+  }, speed * 1000 * 2);
 }
 
-function setMist(opacity){
-    document.querySelector('#mist').style.opacity = opacity;
+function setMist(opacity) {
+  document.querySelector("#mist").style.opacity = opacity;
 }
 
 let cloudDensity = 0.5;
 let cloudSpeed = 20;
 function setConditions(density, speed) {
-    setMist(density);
-    cloudDensity = density;
-    cloudSpeed = speed;
+  setMist(density);
+  cloudDensity = density;
+  cloudSpeed = speed;
 }
 
 setMist(cloudDensity);
 setInterval(() => {
-    if(Math.random() < cloudDensity){
-        addCloud(cloudSpeed);
-    }
+  if (Math.random() < cloudDensity) {
+    addCloud(cloudSpeed);
+  }
 }, 200);
 
+let angle = 0;
+setInterval(() => {
+  angle += 0.004;
+  setSunAngle(angle);
+});
 
-
-let angle = 0
-setInterval(()=>{
-    angle += 0.004;
-    setSunAngle(angle);
-})
-
-function setClock(){
-    let date = new Date();
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    clock.innerHTML = `${hours}:${minutes}`;
+function setClock() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) minutes = "0" + minutes;
+  clock.innerHTML = `${hours}:${minutes}`;
 }
 setClock();
 
 setInterval(() => {
-    setClock();
+  setClock();
 }, 1000);
--- weathers.js
+++ weathers.js
@@ -1,1 +1,31 @@
-console.log("Hello, World!");
+const API_KEY = "3221ba97b0332bd1865ae65794a5c077";
+const API_LINK =
+  "https://api.openweathermap.org/data/2.5/onecall?lat=38.7267&lon=-9.1403&exclude=current,hourly,minutely,alerts&units=metric&appid=";
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch(API_LINK + API_KEY)
+    .then((response) => response.json())
+    .then((weatherData) => {
+      const todayDate = new Date(
+        weatherData.daily[0].dt * 1000
+      ).toLocaleDateString("en");
+
+      const todayTemperature = weatherData.daily[0].temp.day.toFixed(0);
+      const todayWindSpeed = weatherData.daily[0].wind_speed + " mph";
+      const todayHumidity = weatherData.daily[0].humidity;
+
+      console.log(
+        `${todayDate}, ${todayTemperature}, ${todayWindSpeed}, ${todayHumidity}`
+      );
+      weatherData.daily.forEach((eachDay, index) => {
+        if (index > 0) {
+          const dayName = new Date(eachDay.dt * 1000).toLocaleDateString("en", {
+            weekday: "short",
+          });
+          const temperature = eachDay.temp.day.toFixed(0);
+          console.log(`${dayName}, ${temperature}°`);
+        }
+      });
+    })
+    .catch((error) => console.log("Fetch Error: " + error));
+});
