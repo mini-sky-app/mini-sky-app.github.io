b2d1e53a7bdb833a81c6d596b56e09f30c106510
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2022-04-17 09:19:18 -0400

    weathers.js changed to promise

--- weathers.js
+++ weathers.js
@@ -1,56 +1,52 @@
 function getWeatherData() {
-  const weatherDataObj = new Object(); // Main Object
-  const weekWeatherData = new Object();
-  let latitude, longitude; // user
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      latitude = pos.coords.latitude;
-      longitude = pos.coords.longitude;
-      const API_KEY = "3221ba97b0332bd1865ae65794a5c077";
-      const API_LINK = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,hourly,minutely,alerts&units=metric&appid=`;
+  return new Promise((resolve, reject) => {
+    let latitude, longitude; // user
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((pos) => {
+        latitude = pos.coords.latitude;
+        longitude = pos.coords.longitude;
+        const API_KEY = "3221ba97b0332bd1865ae65794a5c077";
+        const API_LINK = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,hourly,minutely,alerts&units=metric&appid=`;
 
-      fetch(API_LINK + API_KEY)
-        .then((response) => response.json())
-        .then((weatherData) => {
-          const todayDate = new Date(
-            weatherData.daily[0].dt * 1000
-          ).toLocaleDateString("en");
+        fetch(API_LINK + API_KEY)
+          .then((response) => response.json())
+          .then((weatherData) => {
+            const weatherDataObj = {}
+            const weekWeatherData = {}
+            const todayDate = new Date(
+              weatherData.daily[0].dt * 1000
+            ).toLocaleDateString("en");
 
-          weatherDataObj.todayWeekDay = new Date(
-            weatherData.daily[0].dt * 1000
-          ).toLocaleDateString("en", { weekday: "short" });
+            weatherDataObj.todayWeekDay = new Date(
+              weatherData.daily[0].dt * 1000
+            ).toLocaleDateString("en", { weekday: "short" });
 
-          weatherDataObj.todayTemp = weatherData.daily[0].temp.day.toFixed(0);
-          weatherDataObj.todayWindSpeed =
-            weatherData.daily[0].wind_speed + " mph";
-          weatherDataObj.todayHumidity = weatherData.daily[0].humidity;
+            weatherDataObj.todayDate = todayDate;
+            weatherDataObj.todayTemp = weatherData.daily[0].temp.day.toFixed(0);
+            weatherDataObj.todayWindSpeed =
+              weatherData.daily[0].wind_speed + " mph";
+            weatherDataObj.todayHumidity = weatherData.daily[0].humidity;
 
-          weatherData.daily.forEach((eachDay, index) => {
-            const dayName = new Date(eachDay.dt * 1000).toLocaleDateString(
-              "en",
-              {
-                weekday: "short",
-              }
-            );
-            const temperature = eachDay.temp.day.toFixed(0);
-            weekWeatherData[dayName] = temperature;
-          });
-        })
-        .catch((error) => console.log("Fetch Error: " + error));
-    });
-  }
-
-  weatherDataObj.weekWeatherData = weekWeatherData;
-  return weatherDataObj; // returns all the weather information needed
+            weatherData.daily.forEach((eachDay) => {
+              const dayName = new Date(eachDay.dt * 1000).toLocaleDateString(
+                "en",
+                {
+                  weekday: "short",
+                }
+              );
+              const temperature = eachDay.temp.day.toFixed(0);
+              weekWeatherData[dayName] = temperature;
+            });
+            weatherDataObj.weekWeatherData = weekWeatherData;
+            resolve(weatherDataObj);
+          })
+          .catch((error) => console.log("Fetch Error: " + error));
+      });
+    }
+  });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  const weatherDataObj = getWeatherData();
-  weekWeather = document.getElementById("week-weather");
-  console.log(weatherDataObj);
-  for (let eachElement = 0; eachElement < 7; eachElement++) {
-    newSpan = document.createElement("span");
-    console.log(Object.keys(weatherDataObj["weekWeatherData"])); // help
-    weekWeather.appendChild(newSpan);
-  }
-});
+getWeatherData().then((weatherData) => {
+  //do things here
+  console.log(weatherData);
+})
