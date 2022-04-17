e3bf457603edb3188891f517431ea213a734aba9
Tony Zhang <61944932+Tony1324@users.noreply.github.com>
2022-04-17 10:22:34 -0400

    show current weather data

--- weathers.js
+++ weathers.js
@@ -6,7 +6,7 @@ function getWeatherData() {
         latitude = pos.coords.latitude;
         longitude = pos.coords.longitude;
         const API_KEY = "3221ba97b0332bd1865ae65794a5c077";
-        const API_LINK = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,hourly,minutely,alerts&units=metric&appid=`;
+        const API_LINK = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,alerts&units=metric&appid=`;
 
         fetch(API_LINK + API_KEY)
           .then((response) => response.json())
@@ -15,21 +15,22 @@ function getWeatherData() {
             const weatherDataObj = {}
             const weekWeatherData = {}
             const todayDate = new Date(
-              weatherData.daily[0].dt * 1000
+              weatherData.current.dt * 1000
             ).toLocaleDateString("en");
 
             weatherDataObj.todayWeekDay = new Date(
-              weatherData.daily[0].dt * 1000
+              weatherData.current.dt * 1000
             ).toLocaleDateString("en", { weekday: "short" });
 
             weatherDataObj.todayDate = todayDate;
-            weatherDataObj.todaySunrise = weatherData.daily[0].sunrise;
-            weatherDataObj.todaySunset = weatherData.daily[0].sunset;
-            weatherDataObj.todayTemp = weatherData.daily[0].temp.day.toFixed(0);
+            weatherDataObj.todaySunrise = weatherData.current.sunrise;
+            weatherDataObj.todaySunset = weatherData.current.sunset;
+            weatherDataObj.todayTemp = weatherData.current.temp.toFixed(0);
             weatherDataObj.todayWindSpeed =
-              weatherData.daily[0].wind_speed + " mph";
-            weatherDataObj.todayHumidity = weatherData.daily[0].humidity;
-            weatherDataObj.todayClouds = weatherData.daily[0].clouds;
+              weatherData.current.wind_speed + " m/s";
+            weatherDataObj.todayHumidity = weatherData.current.humidity;
+            weatherDataObj.todayClouds = weatherData.current.clouds;
+            weatherDataObj.todayVisibility = weatherData.current.visibility;
 
             weatherData.daily.forEach((eachDay) => {
               const dayName = new Date(eachDay.dt * 1000).toLocaleDateString(
@@ -51,9 +52,12 @@ function getWeatherData() {
 }
 
 getWeatherData().then((weatherData) => {
-  //do things here
-  console.log(weatherData);
   setClouds(weatherData.todayClouds / 100, parseInt(weatherData.todayWindSpeed) / 30);
   spawnClouds()  
   setSunTimes(weatherData.todaySunrise, weatherData.todaySunset);
+  setMist(1 - weatherData.todayVisibility / 10000);
+  document.querySelector("#date").innerHTML = `DATE: ${weatherData.todayDate}`;
+  document.querySelector("#temperature").innerHTML = `TEMPERATURE: ${weatherData.todayTemp}°C`;
+  document.querySelector("#wind-speed").innerHTML = `WIND SPEED: ${weatherData.todayWindSpeed}`;
+  document.querySelector("#clouds").innerHTML = `CLOUD COVER: ${weatherData.todayClouds}%`;
 })
