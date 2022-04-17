d2e238184a65982a9643c35793a4861b6bfe9e90
Zhenyu Liang <65879206+Zhenyu-Liang@users.noreply.github.com>
2022-04-16 21:55:43 -0400

    added latitude and longitude and returns weather data

--- weathers.js
+++ weathers.js
@@ -1,31 +1,46 @@
-const API_KEY = "3221ba97b0332bd1865ae65794a5c077";
-const API_LINK =
-  "https://api.openweathermap.org/data/2.5/onecall?lat=38.7267&lon=-9.1403&exclude=current,hourly,minutely,alerts&units=metric&appid=";
+function getWeatherData() {
+  const weatherDataObj = new Object(); // Main Object
+  const weekWeatherData = new Object();
+  let latitude, longitude; // user
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((pos) => {
+      latitude = pos.coords.latitude;
+      longitude = pos.coords.longitude;
+      const API_KEY = "3221ba97b0332bd1865ae65794a5c077";
+      const API_LINK = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=current,hourly,minutely,alerts&units=metric&appid=`;
 
-document.addEventListener("DOMContentLoaded", () => {
-  fetch(API_LINK + API_KEY)
-    .then((response) => response.json())
-    .then((weatherData) => {
-      const todayDate = new Date(
-        weatherData.daily[0].dt * 1000
-      ).toLocaleDateString("en");
+      fetch(API_LINK + API_KEY)
+        .then((response) => response.json())
+        .then((weatherData) => {
+          const todayDate = new Date(
+            weatherData.daily[0].dt * 1000
+          ).toLocaleDateString("en");
 
-      const todayTemperature = weatherData.daily[0].temp.day.toFixed(0);
-      const todayWindSpeed = weatherData.daily[0].wind_speed + " mph";
-      const todayHumidity = weatherData.daily[0].humidity;
+          weatherDataObj.todayTemp = todayTemperature =
+            weatherData.daily[0].temp.day.toFixed(0);
+          weatherDataObj.todayWindSpeed =
+            weatherData.daily[0].wind_speed + " mph";
+          weatherDataObj.todayHumidity = weatherData.daily[0].humidity;
 
-      console.log(
-        `${todayDate}, ${todayTemperature}, ${todayWindSpeed}, ${todayHumidity}`
-      );
-      weatherData.daily.forEach((eachDay, index) => {
-        if (index > 0) {
-          const dayName = new Date(eachDay.dt * 1000).toLocaleDateString("en", {
-            weekday: "short",
+          weatherData.daily.forEach((eachDay, index) => {
+            if (index > 0) {
+              const dayName = new Date(eachDay.dt * 1000).toLocaleDateString(
+                "en",
+                {
+                  weekday: "short",
+                }
+              );
+              const temperature = eachDay.temp.day.toFixed(0);
+              weekWeatherData[dayName] = temperature;
+            }
           });
-          const temperature = eachDay.temp.day.toFixed(0);
-          console.log(`${dayName}, ${temperature}°`);
-        }
-      });
-    })
-    .catch((error) => console.log("Fetch Error: " + error));
-});
+        })
+        .catch((error) => console.log("Fetch Error: " + error));
+    });
+  }
+
+  weatherDataObj.weekWeatherData = weekWeatherData;
+  return weatherDataObj; // returns all the weather information needed
+}
+
+document.addEventListener("DOMContentLoaded", () => getWeatherData());
