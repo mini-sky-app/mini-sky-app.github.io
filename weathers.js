46d70daac510cfaabf98e25907a465ce76b2dcfa
Zhenyu Liang <65879206+Zhenyu-Liang@users.noreply.github.com>
2022-04-17 20:10:14 -0400

    added the user city location and unit select (imperial and metric)

--- weathers.js
+++ weathers.js
@@ -1,6 +1,7 @@
 function getWeatherData() {
   return new Promise((resolve, reject) => {
     let latitude, longitude; // user
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((pos) => {
         latitude = pos.coords.latitude;
@@ -8,10 +9,32 @@ function getWeatherData() {
         const API_KEY = "3221ba97b0332bd1865ae65794a5c077";
         const API_LINK = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,alerts&units=metric&appid=`;
 
+        const xhr = new XMLHttpRequest();
+        xhr.open(
+          "GET",
+          "https://us1.locationiq.com/v1/reverse.php?key=pk.93282e72d3c4c7d23e602a8cca1d469a	&lat=" +
+            latitude +
+            "&lon=" +
+            longitude +
+            "&format=json",
+          true
+        );
+        xhr.send();
+        xhr.onreadystatechange = processReq;
+        xhr.addEventListener("readystatechange", processReq, false);
+
+        function processReq(e) {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            const res = JSON.parse(xhr.responseText);
+            const city = res.address.city;
+            document.getElementById("city").innerHTML = city;
+            return;
+          }
+        }
+
         fetch(API_LINK + API_KEY)
           .then((response) => response.json())
           .then((weatherData) => {
-            console.log(weatherData);
             const weatherDataObj = {};
             const weekWeatherData = {};
             const todayDate = new Date(
@@ -53,7 +76,7 @@ function getWeatherData() {
   });
 }
 
-function setWeatherData() {
+function setWeatherData(unit, changed) {
   getWeatherData().then((weatherData) => {
     setClouds(
       weatherData.todayClouds / 100,
@@ -71,9 +94,11 @@ function setWeatherData() {
       // exclude today
       let newSpan = document.createElement("span");
       let eachDayName = weekWeatherDataKeys[eachDay];
-      let weatherValue = `${eachDayName.toUpperCase()} ${
-        weatherData.weekWeatherData[eachDayName]
-      }°`;
+      let eachDayTemperature = weatherData.weekWeatherData[eachDayName];
+      if (changed == 1 && unit == "F") {
+        eachDayTemperature = (1.8 * eachDayTemperature + 32).toFixed(0);
+      }
+      let weatherValue = `${eachDayName.toUpperCase()} ${eachDayTemperature}°`;
 
       if (eachDay !== 4) {
         // if not the last day
@@ -83,12 +108,19 @@ function setWeatherData() {
       newSpan.textContent = weatherValue;
       weekWeather.appendChild(newSpan);
     }
+
+    let todayTemperature = weatherData.todayTemp;
+
+    if (changed == 1 && unit == "F") {
+      todayTemperature = (1.8 * todayTemperature + 32).toFixed(0);
+    }
+
     document.querySelector(
       "#date"
     ).innerHTML = `DATE: ${weatherData.todayDate}`;
     document.querySelector(
       "#temperature"
-    ).innerHTML = `TEMPERATURE: ${weatherData.todayTemp}°C`;
+    ).innerHTML = `TEMPERATURE: ${todayTemperature}°${unit}`;
     document.querySelector(
       "#wind-speed"
     ).innerHTML = `WIND SPEED: ${weatherData.todayWindSpeed}`;
@@ -98,8 +130,33 @@ function setWeatherData() {
   });
 }
 
-setWeatherData();
+let unit = "C";
+let changed = 0;
+const celsiusBtn = document.getElementById("celsius");
+const fahrenheitBtn = document.getElementById("fahrenheit");
+
+celsiusBtn.addEventListener("click", () => {
+  if (unit == "F") {
+    unit = "C";
+    changed = 1;
+    celsiusBtn.style.opacity = "1.0";
+    fahrenheitBtn.style.opacity = "0.44";
+    setWeatherData(unit, changed);
+  }
+});
+
+fahrenheitBtn.addEventListener("click", () => {
+  if (unit == "C") {
+    unit = "F";
+    changed = 1;
+    celsiusBtn.style.opacity = "0.44";
+    fahrenheitBtn.style.opacity = "1.0";
+    setWeatherData(unit, changed);
+  }
+});
+
+setWeatherData(unit, 0);
 
 setInterval(() => {
-  setWeatherData();
+  setWeatherData(unit, 0);
 }, 20 * 60 * 1000);
